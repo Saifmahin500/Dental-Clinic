54923a5b9dd320e65ff1591d2d799118e3f4aee0
saifmahin500 <saifmahin500@gmail.com>
2023-10-07 12:31:48 +0600

    first commit

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC11oK9II1zy8lcmj-uitR9svFCFh2981Y",
+  authDomain: "th-assignment-4445a.firebaseapp.com",
+  projectId: "th-assignment-4445a",
+  storageBucket: "th-assignment-4445a.appspot.com",
+  messagingSenderId: "437486988290",
+  appId: "1:437486988290:web:3c1463d973d994f77dcb1b"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
